9512b4c72d90681ac057dff9708738242703c37d
juhlig <j.uhlig@mailingwork.de>
2019-02-13 12:14:35 +0100

    phone-number: update exercise

--- exercises/phone-number/src/example.erl
+++ exercises/phone-number/src/example.erl
@@ -1,38 +1,15 @@
 -module(example).
 
--export([number/1, areacode/1, pretty_print/1, test_version/0]).
+-export([clean/1]).
 
--define(ZEROS, "0000000000").
+clean(String) ->
+    String1=normalize(String),
+    String2=sanitize1(String1),
+    sanitize2(String2).
 
-number(String) ->
-  Digits = digits(String),
-  case length(Digits) of
-    10 -> Digits;
-    11 -> strip_one(Digits);
-    _ -> ?ZEROS
-  end.
+normalize(String) -> [C || C <- String, C>=$0, C=<$9].
 
-% Strips a leading '1' off the front of a string.
-strip_one([49|T]) -> T;
-strip_one([_P|_T]) -> ?ZEROS.
+sanitize1(String) when length(String)=:=10 -> String;
+sanitize1([$1|String]) when length(String)=:=10 -> String.
 
-parts(Number) ->
-  PhoneNumber = number(Number),
-  AreaCode = string:sub_string(PhoneNumber, 1, 3),
-  Exchange = string:sub_string(PhoneNumber, 4, 6),
-  Subscriber = string:sub_string(PhoneNumber, 7, 10),
-  {AreaCode, Exchange, Subscriber}.
-
-digits(String) ->
-  re:replace(String, "\\D", "", [global, {return, list}]).
-
-areacode(String) ->
-  {AreaCode, _, _} = parts(String),
-  AreaCode.
-
-pretty_print(String) ->
-  {AreaCode, Exchange, Subscriber} = parts(String),
-  lists:flatten(io_lib:format("\(~s\) ~s-~s", [AreaCode, Exchange, Subscriber])).
-
-test_version() ->
-    1.
+sanitize2(String=[A, _, _, D|_]) when A=/=$0, A=/=$1, D=/=$0, D=/=$1 -> String.
--- exercises/phone-number/src/phone_number.erl
+++ exercises/phone-number/src/phone_number.erl
@@ -1,14 +1,6 @@
 -module(phone_number).
 
--export([number/1, areacode/1, pretty_print/1, test_version/0]).
+-export([clean/1]).
 
-number(_String) ->
-  undefined.
 
-areacode(_String) ->
-  undefined.
-
-pretty_print(_String) ->
-  undefined.
-
-test_version() -> 1.
+clean(_String) -> undefined.
